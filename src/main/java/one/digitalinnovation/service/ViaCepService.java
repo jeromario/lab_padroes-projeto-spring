afe67895e1e72e71a0effcbfebb4064641b35578
origin <https://github.com/jeromario/desafio_poo.git>
2023-03-08 10:15:04 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/service/ViaCepService.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.service;
+
+import one.digitalinnovation.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@FeignClient(name = "cep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
